4197818ca1ec7fce39b27d9ce34961c09ddb5e49
Basil-NB <120618274+Basil-NB@users.noreply.github.com>
2023-03-31 15:15:56 -0400

    added array

--- Develop/index.js
+++ Develop/index.js
@@ -2,10 +2,11 @@
 const inquirer = require('inquirer');
 const genMark = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-//const questions = ['What was your motivation?', 'Why did you build this project?', 'What problem does it solve?', 'What did you learn?', 'What makes your project stand out?'];
-const questions = [{type:'input', name:'motivation', message:'What was your motivation?'}] 
+const questions = [{type:'input', name:'motivation', message:'What was your motivation?'}, {type:'imput', name:'why', message:'Why did you build this project?'}, {type:'imput', name:'problem', message:'What problem does it solve?'}, {type:'imput', name:'learn', message:'What did you learn?'}, {type:'imput', name:'stand-out', message:'What makes your project stand out?'}]; 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 
 // TODO: Create a function to initialize app
